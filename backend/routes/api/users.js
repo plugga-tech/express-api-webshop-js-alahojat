a15076ef4ea12270434ad8fa09a5be64e565050f
Ala Hojat Jalali <alahojatjalali@gmail.com>
2024-02-04 21:03:29 +0100

    ✨ feat(users.js) find ID for specific user with POST

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,14 +5,37 @@ var router = express.Router();
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
 
-  req.app.locals.db.collection("users").find().toArray()
+  let projection = { password: 0};
+
+  req.app.locals.db.collection("users").find({}, { projection: projection}).toArray()
   .then (result => {
     console.log(result);
     res.send(result);    
   })
 });
+
+
+
+router.post("/", (req, res) => {
+  let userId = req.body.id;
+
+  req.app.locals.db.collection("users").findOne({ id: userId })
+    .then(ifIdExists => {
+      // If user is found in MongoDB, send it in the response
+      if (ifIdExists) {
+        res.json(ifIdExists);
+      } else {
+        // If not found in MongoDB, return a 404 status
+        res.status(404).json({ message: "No such user in the database!" });
+       
+      }
+    })
+   
+});
+
+
 
 module.exports = router;
 
