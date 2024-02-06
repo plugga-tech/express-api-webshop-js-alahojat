231dad7bb896f5d7623d8cddeeb3b5d213abfe00
Ala Hojat Jalali <alahojatjalali@gmail.com>
2024-02-06 08:32:07 +0100

    ✨feat(orders.js create GET to get all orders once posted

--- backend/routes/api/orders.js
+++ backend/routes/api/orders.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const { ObjectId } = require('mongodb');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+
+router.post('/add', async (req, res) => {
+  const userId = req.body.userId;
+  const products = req.body.products;
+
+  const customer = await req.app.locals.db.collection("users").findOne({ _id: new ObjectId(userId) });
+
+  if (!customer) {
+    res.status(401).json({ message: "This user does not exist in the database"});
+  };
+
+  const newCustomerOrder = {
+    userId: new ObjectId(userId),
+    products: products 
+  };
+
+  await req.app.locals.db.collection("orders").insertOne(newCustomerOrder);
+
+  res.redirect('/api/orders/all');
+  // redirect to orders/all
+  console.log(customer);
+});
+
+  
+
+// Get all orders for customers
+router.get('/all', async (req, res) => {
 
   req.app.locals.db.collection("orders").find().toArray()
   .then (result => {
@@ -10,6 +36,9 @@ router.get('/', function(req, res, next) {
     res.send(result);    
   })
 });
+
+
+
 
 
 
