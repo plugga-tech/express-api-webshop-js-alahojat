12893232cac65c430d2332f837fa2d7a5a712db8
Ala Hojat Jalali <alahojatjalali@gmail.com>
2024-02-11 20:45:30 +0100

    🎨 feat(products.js) clean up in code

--- backend/routes/api/products.js
+++ backend/routes/api/products.js
@@ -2,48 +2,46 @@ var express = require('express');
 var router = express.Router();
 const { ObjectId } = require('mongodb');
 
-/* GET users listing. */
+// Get all products
 router.get('/', function(req, res, next) {
 
   req.app.locals.db.collection("products").find().toArray()
   .then (result => {
-    console.log(result);
     res.send(result);    
+  })
+  .catch(error => {
+    console.error("Error getting products:", error);
+    res.status(400).json({ error: "Failed to get all products" });
   })
 });
 
-
-// GET product specific ID
+// Get product specific ID
 router.get("/:id", (req, res) => {
   const productId = req.params.id;
 
-   req.app.locals.db.collection("products")
-   .findOne({ _id: new ObjectId(productId) })
-    .then(ifProductIdExists => {
-      // If user is found in MongoDB, send it in the response
-      if (ifProductIdExists) {
-        res.json(ifProductIdExists);
-      } else {
-        // If not found in MongoDB, return a 404 status
-        res.status(404).json({ message: "This product does not exist!" });
-       
-      }
-    })
-   
+  req.app.locals.db.collection("products")
+  .findOne({ _id: new ObjectId(productId) })
+  .then(ifProductIdExists => {
+    if (ifProductIdExists) {
+      res.json(ifProductIdExists);
+    } else {
+      res.status(404).json({ message: "This product does not exist!" });
+    }
+  })
 });
 
-// CREATE A NEW PRODUCT
-
+// Create a new product
 router.post("/add", (req, res) => {
-
   let newProduct = req.body;
   
   req.app.locals.db.collection("products").insertOne(newProduct)
   .then(() => {
-    console.log(newProduct);
     res.json(newProduct);
   })
+  .catch(error => {
+    console.error("Error creating new product:", error);
+    res.status(400).json({ error: "Unable to create a new product" });
+  });
 });
-
 
 module.exports = router;
